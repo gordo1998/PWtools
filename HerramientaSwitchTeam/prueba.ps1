ded4e7bffcd102a6cc7537ba5eee558217903acd
jordi barreda alvarez <jordi@jordi-w11.dominio.lan>
2024-03-13 10:47:44 +0100

    tercer commit

--- HerramientaSwitchTeam/prueba.ps1
+++ HerramientaSwitchTeam/prueba.ps1
@@ -1,12 +1,12 @@
 
-$menu = "opciones:`n1) Listar adaptadores de red`n2)Crear nuevo SwitchTeam`n3)Eliminar SwitchTeam`n"
+$menu = "opciones:`n1) Listar adaptadores de red`n2) Crear nuevo SwitchTeam`n3) Eliminar SwitchTeam`n"
 $Host.UI.RawUI.ForegroundColor = 'Yellow'
 $menu
 
-do
+do#Itera hasta que se ponga un valor válido
 {
 	$opcionMenu = Read-host -prompt "Ingresa un numero de seleccion"
-
+	#si opcionMenu esta vacio vuelve a preguntar
 	if ([string]::IsNullOrEmpty($opcionMenu)) {
 		$Host.UI.RawUI.ForegroundColor = 'Red'
 		Write-Host "Debe introducir un valor."
@@ -15,12 +15,14 @@ do
 }
 until($opcionMenu -match '^\d+$' )
 
-if ($opcionMenu -eq 1)
+#Se puede intentar realizat todo este bloque if else con un switch
+#Este bloque ejecuta todo el programa principal
+if ($opcionMenu -eq 1)#Lista todos los adaptadores
 {
-	$array = Get-NetAdapter
-	$ColumName = $array | Select-Object -ExpandProperty Name
+	$array = Get-NetAdapter #Guarda el comando para listar los adaptadores
+	$ColumName = $array | Select-Object -ExpandProperty Name #Extrae unicamente los nombres
 	$columName
-}elseif ($opcionMenu -eq 2)
+}elseif ($opcionMenu -eq 2)# Crea un nuevo equipo
 {
 	Write-host "Elija al menos dos adaptadores..."
 	$array = Get-NetAdapter #Coge toda la tabla
@@ -31,7 +33,7 @@ if ($opcionMenu -eq 1)
 
 	for($i = 0; $i -lt $arrayDos.count; $i = $i + 1) #Listamos la matriz con su índice para que escoja la opción
 	{
-		"$i)$($arrayDos[$i])"
+		"$i)$($arrayDos[$i])" #mostramos en pantalla las opciones
 	}
 
 	do{
@@ -57,25 +59,25 @@ if ($opcionMenu -eq 1)
 	
 	
 		
-	$adaptadores = New-Object System.Collections.ArrayList
-	$adapterNoExist = $false
+	$adaptadores = New-Object System.Collections.ArrayList #Aquí se ubicarán todos los adaptadores
+	$adapterExist = $false # Utilizamos esta variable para saber si el adaptador esta disponible o no
 	for ($y = 0; $y -lt $cantAdaptadores; $y = $y + 1)
 	{
 		do {
 			$adapter = Read-host -prompt "Elije el adaptador"
-			$adapterNoExist = ipconfig | Select-String -Pattern "\b$($arrayDos[$adapter])\b" -Quiet
+			$adapterExist = ipconfig | Select-String -Pattern "\b$($arrayDos[$adapter])\b" -Quiet #Devuelve verdadero si existe
 			Write-Host "adaptador: $adapterNoExist"
-			if ($adapterNoExist -ne $true){
+			if ($adapterExist -ne $true){
 				Write-Host "El adapatador ya se encuentra en un equipo!"
 			}
-		}until($adapter -match '^\d+$' -and $adapterNoExist -eq $true)
+		}until($adapter -match '^\d+$' -and $adapterExist -eq $true)#saldrá del bucle si el adaptador está disponible
 		
 		$adaptadores.Add($arrayDos[$adapter])
 	}
 	
 	do {
 		$nameTeam = [String] (Read-host -prompt "Ahora elije el nombre de grupo")
-		if(($arrayDos -contains $nameTeam)){
+		if(($arrayDos -contains $nameTeam)){  # Si el nombre de equipo ya existe le diremos que no puede
 			$Host.UI.RawUI.ForegroundColor = 'Red'
 			Write-Host "Ya existe este nombre equipo!!"
 			$Host.UI.RawUI.ForegroundColor = 'Yellow'
@@ -86,13 +88,13 @@ if ($opcionMenu -eq 1)
 	
 	$adaptadores = $adaptadores.ToArray([String])
 
-	New-NetSwitchTeam -Name $nameTeam -TeamMembers $adaptadores
+	New-NetSwitchTeam -Name $nameTeam -TeamMembers $adaptadores #Crea el Switch team
 			
 		
 
 }elseif($opcionMenu -eq 3)
 {
 	$nombreAdaptador = Read-Host -Prompt "Indique el nombre del equipo"
-	Remove-NetSwitchTeam -Name $nombreAdaptador
+	Remove-NetSwitchTeam -Name $nombreAdaptador #Borra el Switch team
 }
 
